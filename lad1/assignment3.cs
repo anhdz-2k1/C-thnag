98629528a15fee3b669ba4dee9331aa4efaacdac
Anh Nguyen <anhmanchester2k1@gmail.com>
2020-05-10 00:46:57 +0700

    lad1

---
+++ lad1/assignment3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace assignment3
+{
+    class assignment3
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so ban chon : ");
+            n = int.Parse(Console.ReadLine());
+            if (n >  1 && n <  9)
+            {
+                if(n == 2)
+                {
+                    Console.WriteLine("Thu hai");
+                } 
+                else if( n == 3){
+                    Console.WriteLine("Thu ba");
+                }
+                else if (n == 4)
+                {
+                    Console.WriteLine("Thu tu");
+                }
+                else if (n == 5)
+                {
+                    Console.WriteLine("Thu nam");
+                }
+                else if (n == 6)
+                {
+                    Console.WriteLine("Thu sau");
+                }
+                else if (n == 7)
+                {
+                    Console.WriteLine("Thu bay");
+                }
+                else if (n == 8)
+                {
+                    Console.WriteLine("Chu nhat");
+                }
+            }
+            else {
+                Console.WriteLine("Day khong phai mot ngay trong tuan");
+            }
+            
+        }
+    }
+}
---
+++ lad1/assignment4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace assignment4
+{
+    class assignment4
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so ban chon : ");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 1; i < 10; i++)
+            {
+                int boiso = n * i;
+                Console.WriteLine("Bo so cua so ban nhap la {0}", boiso);
+            }
+        }
+    }
+}
