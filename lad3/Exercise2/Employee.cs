b9e9925bccec9aabf24b1bafa2e7d994ee300569
Anh Nguyen <anhmanchester2k1@gmail.com>
2020-05-14 17:52:15 +0700

    lad3

---
+++ lad3/Exercise2/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exescise2
+{
+    class Employee
+    {
+             protected string firstName;
+             protected string lastName;
+             protected string address;
+             protected long sin;
+             protected double salary;
+        public Employee(string firstName, string lastName, string address, long sin, double salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.sin = sin;
+            this.salary = salary;
+        }
+        public virtual void Salary(double discount)
+        {
+            
+            Console.Write("Your salary is : ");
+            Console.WriteLine(salary * discount/100);
+        }
+        public override string ToString()
+        {
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(address);
+            Console.WriteLine(sin);
+            Console.WriteLine(salary);
+            return base.ToString();
+        }
+
+         
+    }
+}
---
+++ lad3/Exescise1/Atom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exescise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             long number = 0,n;
+             int i = 0;
+            string symbol = null, fullname = null;
+            double weight = 0.000;
+            Console.Write("Atomic Information ");
+            Console.WriteLine("==================");
+            Console.Write("Enter the prime number you want to add:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i = 0; i < n; i++)
+            {
+                Console.Write("Enter atomic number: ");
+                number = Convert.ToInt16(Console.ReadLine());
+                
+                if(number == 0)
+                {
+                    Console.WriteLine("No Sym Name Weigh");
+                }  else if(number > 0)
+                {
+                    Console.Write("Enter symbol : ");
+                    symbol = Console.ReadLine();
+                    Console.Write("Enter full name : ");
+                    fullname = Console.ReadLine();
+                    Console.Write("Enter atomic weight: ");
+                    weight = Convert.ToSingle(Console.ReadLine());
+                    Console.Write(number);
+                    Console.Write(" {0:n}", symbol);
+                    Console.Write(" {0:n}", fullname);
+                    Console.WriteLine(" {0:n}", weight);
+
+                }  
+            }           
+        }
+    }
+}
