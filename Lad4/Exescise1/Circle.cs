dc44d44775ce51cf21c1ea0736ba2cd362b83747
Anh Nguyen <anhmanchester2k1@gmail.com>
2020-05-17 21:11:06 +0700

    lad4

---
+++ Lad4/Exescise1/Circle.cs
@@ -0,0 +1,33 @@
+﻿using Lad4;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exescise1
+{
+    public class Circle : GeometricObject
+    {
+        private double radius;
+        public Circle(double x)
+        {
+            this.radius = x;
+        }
+        public Circle(double x, string c, double w) : base(c, w)
+        {
+            this.radius = x;
+        }
+        public override string ToString()
+        {
+            return "Circle has: radius is " + radius + ", color is " + PColor + ", weight is " + PWeight;
+        }
+
+        public override double findArea()
+        {
+            return Math.PI * radius * radius;
+        }
+        public override double findPerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+    }
+}
