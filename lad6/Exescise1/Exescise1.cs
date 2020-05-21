999169e6ceb26f34f18b1dcc3a75a95f6d817b48
Anh Nguyen <anhmanchester2k1@gmail.com>
2020-05-21 17:26:12 +0700

    lad6

---
+++ lad6/Exescise1/Exescise1.cs
@@ -0,0 +1,70 @@
+﻿using Customer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer
+{
+    class Cust_details
+
+    {
+        public string strNAme;
+        public void getName()
+        {
+            Console.WriteLine("Enter your name :");
+            strNAme = Console.ReadLine();
+        }
+
+    }
+}
+namespace Order
+{
+    class Grocety_items
+    {
+        public void Ord_grocery()
+        {
+            Cust_details objCust1 = new Cust_details();
+            objCust1.getName();
+            Console.WriteLine("Hello {0} ", objCust1.strNAme);
+            Console.WriteLine("You  have ordered grocery items");
+        }
+    }
+    class Bakery_items
+    {
+        public void Ord_bakery()
+        {
+            Cust_details objCust2 = new Cust_details();
+            objCust2.getName();
+            Console.WriteLine("Hello {0} ", objCust2.strNAme);
+            Console.WriteLine("You  have ordered grocery items");
+        }    
+    }
+    class OrderTest
+    {
+        public static void Main()
+        {
+            string choice;
+            Console.WriteLine("What would you like to order? 1-Grocery Items, 2 - Bakery Items");
+            choice = Console.ReadLine();
+            if(choice == "1")
+            {
+                Grocety_items objGrocery = new Grocety_items();
+                objGrocery.Ord_grocery();
+            }
+            else
+            {
+                if(choice == "2")
+                {
+                    Bakery_items objBakery = new Bakery_items();
+                    objBakery.Ord_bakery();
+
+                }
+                else
+                {
+                    Console.WriteLine("Enter either 1 or 2 ");
+                }
+            }
+            Console.ReadLine();
+        }   
+    }
+}
---
+++ lad6/ex1/Customer.cs
@@ -0,0 +1,131 @@
+﻿using Customer;
+using Order;
+using System;
+using System.Globalization;
+
+namespace Customer
+{
+    class addName
+    {
+        public string Name;
+        public void getName()
+        {
+            Console.Write("Enter your name :");
+            Name = Console.ReadLine();
+            Console.WriteLine("Your name is: " + Name);
+        }
+    }
+}
+namespace Order
+{
+    class Order1
+    {
+
+        public void Grocety()
+        {
+            int n;
+            Console.WriteLine("Danh sach cac mat hang tap hoa: ");
+            Console.WriteLine("1-Bim bim: ");
+            Console.WriteLine("2-Snake ");
+            Console.WriteLine("3-Cocacola ");
+            Console.WriteLine("4-Pepsi ");
+
+            do
+            {
+
+                do
+                {
+                    Console.Write("Nhap id mat pham ba muon mua: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+
+                } while (n < 0 || n > 4);
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("Ban da mua thang con bim bim");
+                        break;
+                    case 2:
+                        Console.WriteLine("Ban da mua thanh cong Snake");
+                        break;
+                    case 3:
+                        Console.WriteLine("Ban da mua thanh cong Cocacola");
+                        break;
+                    case 4:
+                        Console.WriteLine("Ban da mua thanh cong Pepsi");
+                        break;
+
+                }
+            } while (n != 0);
+        }
+        public void Bakery()
+        {
+            int n;
+            Console.WriteLine("Danh sach cac mat hang tap hoa: ");
+            Console.WriteLine("1-Banh my bate ");
+            Console.WriteLine("2-Banh my trung ");
+            Console.WriteLine("3-Banh my xuc xich ");
+            Console.WriteLine("4-Banh my trung xuc xich ");
+
+            do
+            {
+                do
+                {
+                    Console.Write("Nhap id mat pham ba muon mua: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+
+                } while (n < 0 || n > 4);
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("Ban da mua thang con bim bim");
+                        break;
+                    case 2:
+                        Console.WriteLine("Ban da mua thanh cong Snake");
+                        break;
+                    case 3:
+                        Console.WriteLine("Ban da mua thanh cong Cocacola");
+                        break;
+                    case 4:
+                        Console.WriteLine("Ban da mua thanh cong Pepsi");
+                        break;
+                }
+            } while (n != 0);
+        }
+    }
+}
+namespace Mainnnn{
+    class Mainnn
+    {
+        public static void Main()
+        {
+            addName add1 = new addName();
+            add1.getName();
+            int n;
+            Order1 o1 = new Order1();
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Danh sanh:");
+                    Console.WriteLine("1-Tap hoa");
+                    Console.WriteLine("2-Banh my");
+                    Console.WriteLine("0-Ket thuc");
+                    Console.Write("Nhap id mat pham ba muon mua: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+
+                } while (n < 0 || n > 2);
+                switch (n)
+                {
+                    case 1: o1.Grocety();
+                        break;                        
+                    case 2:
+                        o1.Bakery();
+                        break;                 
+                }
+            } while (n != 0);
+
+        }
+    }
+}
+
+
