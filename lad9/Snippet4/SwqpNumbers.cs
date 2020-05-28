f75e766c022bcdfc187bf46eb706667e76bdfa3b
Anh Nguyen <anhmanchester2k1@gmail.com>
2020-05-28 16:10:05 +0700

    lad9

---
+++ lad9/Snippet4/SwqpNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snippet4
+{
+    class SwqpNumbers
+    {
+        static void Swap<T>(ref T valOne, ref T valTwo)
+        {
+            T temp = valOne; 
+            valOne = valTwo; 
+            valTwo = temp;
+        }
+        
+        static void Main(string[] args)
+        {
+            int numOne = 23; 
+            int numTwo = 45; 
+            Console.WriteLine("Values before swapping: " +numOne + " & " +numTwo);
+            Swap<int>(ref numOne, ref numTwo);
+            Console.WriteLine("Values after swapping: " + numOne + " & " +numTwo);
+        }
+    }
+}
